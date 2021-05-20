060ad6ada7bee6c6d1ef3f8df78ae5fffeae0ccb
Alan King <alanking@renci.org>
2021-05-20 13:35:32 -0400

    print path

--- irods_consortium_continuous_integration_build_hook.py
+++ irods_consortium_continuous_integration_build_hook.py
@@ -44,7 +44,8 @@ def install_building_dependencies(externals_directory):
         os_specific_directory = irods_python_ci_utilities.append_os_specific_directory(externals_directory)
         externals = []
         for irods_externals in externals_list:
-            externals.append(glob.glob(os.path.join(os_specific_directory, irods_externals + '*.{0}'.format(package_suffix)))[0])
+            external_path = os.path.join(os_specific_directory, irods_externals + '*.{0}'.format(package_suffix))
+            externals.append(glob.glob(external_path)[0])
         irods_python_ci_utilities.install_os_packages_from_files(externals)
 
     add_cmake_to_front_of_path()
